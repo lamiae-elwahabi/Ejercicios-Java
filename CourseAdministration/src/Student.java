5b602a25430986a164cff1dd2a3bfaaf296b8b33
lamiae-elwahabi <lamiaewahabi@hotmail.es>
2017-01-19 12:49:03 +0100

    Add ejercicios practica Java

---
+++ CourseAdministration/src/Student.java
@@ -0,0 +1,22 @@
+
+public class Student {
+	int id;
+	String firstName;
+	String lastName;
+	int birthYear, birthMonth, birthDay;
+	
+	boolean isBirthday () {
+		// Return true if it's the student's birthday today.
+		return false;
+	}
+	
+	void giveWarning(boolean isFinalWarning) {
+		// You should study harder!
+	}
+	
+	int numberOfFriends () {
+		// Return the number of friends the student has.
+		return 0;
+	}
+	
+}
