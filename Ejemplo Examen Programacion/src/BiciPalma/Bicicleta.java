41b19a257dad81f0018eb35b7668ce6844f6af1e
LAMIAE EL WAHABI <lamiaewahabi@hotmail.es>
2017-02-08 15:49:12 +0100

    Update Bicicleta.java

--- Ejemplo Examen Programacion/src/BiciPalma/Bicicleta.java
+++ Ejemplo Examen Programacion/src/BiciPalma/Bicicleta.java
@@ -7,9 +7,21 @@ public class Bicicleta {
 	
 	private int id = 0;
 	
-	//Constructor de la clase 
+	//Constructores de la clase 
+	
+	public Bicicleta(){
+	}
 	
 	public Bicicleta(int id){
 		this.id = id;
+	}
+	
+	//Getters y setters
+	public int getId(){
+		return this.id;
+	}
+	
+	public void setId(int id){
+			this.id = id;
 	}
 }
