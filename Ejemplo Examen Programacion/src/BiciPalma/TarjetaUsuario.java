60f9603a8be2e5c311a3128e764dae4608f5df01
LAMIAE EL WAHABI <lamiaewahabi@hotmail.es>
2017-02-08 15:50:04 +0100

    Update TarjetaUsuario.java

--- Ejemplo Examen Programacion/src/BiciPalma/TarjetaUsuario.java
+++ Ejemplo Examen Programacion/src/BiciPalma/TarjetaUsuario.java
@@ -4,16 +4,28 @@ public class TarjetaUsuario {
 	
 	//Propiedad de la clase 
 	
-		private int id = 000000000;
-		private Boolean activada = true;
+		private String id = "000000000";
+		private Boolean estado = true;
 		
 	//Constructor de la clase 
-		
-		public TarjetaUsuario (int id, Boolean estado Tarjeta){
-				this.id = id;
-				this.activada = activada;
+		public TarjetaUsuario(){
 		}
 		
 		
-
+		public TarjetaUsuario (String id, Boolean true){
+				this.id = id;
+				this.estado = estado;
+		}
+		
+	//Getters y setters
+		public String getId(){
+				return this.id;
+		}
+		public void setId(String id){
+				this.id = id;
+		}
+		public Boolean getEsatdo(){
+				return this.estado
+		}
+		
 }
