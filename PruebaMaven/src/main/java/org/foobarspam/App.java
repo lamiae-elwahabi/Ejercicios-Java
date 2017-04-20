c018ed57ff5ce0bf53ae111697cf5899542efbee
lamiae-elwahabi <lamiaewahabi@hotmail.es>
2017-04-20 23:58:03 +0200

    Nuevo proyecto prueba Maven

---
+++ PruebaMaven/src/main/java/org/foobarspam/App.java
@@ -0,0 +1,8 @@
+package org.foobarspam;
+
+public class App {
+	//Metodo que no recibe ningun parametro 
+	public String Sample(){
+		return "sample";
+	}
+}
---
+++ PruebaMaven/src/test/java/org/foobarspam/AppTest.java
@@ -0,0 +1,14 @@
+package org.foobarspam;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+
+	}
+
+}
