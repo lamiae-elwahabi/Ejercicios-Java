ad920e1b03a13541b9258c19c34b96512c08a95a
LAMIAE EL WAHABI <lamiaewahabi@hotmail.es>
2017-06-07 07:22:33 +0200

    Add files via upload

---
+++ PatternMatcher/PatternMatcher/RegexMatch.java
@@ -0,0 +1,34 @@
+package PatternMatcher;
+
+//RegexMatch.java
+
+public class RegexMatch {
+	public static void main(String[] args) {
+		//Prepare a regular expression to represent a pattern
+		String regex = ".@.";
+		
+		//Try matching many strings against the regular expression
+		RegexMatch.matchIt("a@k", regex);
+		RegexMatch.matchIt("webmaster@jdojo.com", regex);
+		RegexMatch.matchIt("r@j", regex);
+		RegexMatch.matchIt("a%N", regex);
+		RegexMatch.matchIt(".@.", regex);
+		RegexMatch.matchIt("lamiaewahabi@hotmail.es", regex);
+		RegexMatch.matchIt("d@c", regex);
+		RegexMatch.matchIt("meriam@hotmail.es", regex);
+		RegexMatch.matchIt("8@7", regex);
+		RegexMatch.matchIt("deno@deno", regex);
+				
+			
+	
+	}
+	public static void matchIt(String str, String regex) {
+		//Test for pattern match
+		if (str.matches(regex)) {
+			System.out.println(str + " matches the regex " + regex);
+		}
+		else {
+			System.out.println(str + " does not match the regex " + regex);
+		}
+	}
+}
